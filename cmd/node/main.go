b75d9acd138d2bc6ed1219a8a1be9bc852ef74ea
iulianpascalau <iulian.pascalau@gmail.com>
2019-11-25 10:27:29 +0200

    created mock application

---
+++ cmd/node/main.go
@@ -0,0 +1,302 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/urfave/cli"
+)
+
+var (
+	nodeHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+USAGE:
+   {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}
+   {{if len .Authors}}
+AUTHOR:
+   {{range .Authors}}{{ . }}{{end}}
+   {{end}}{{if .Commands}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}
+VERSION:
+   {{.Version}}
+   {{end}}
+`
+
+	// genesisFile defines a flag for the path of the bootstrapping file.
+	genesisFile = cli.StringFlag{
+		Name:  "genesis-file",
+		Usage: "The node will extract bootstrapping info from the genesis.json",
+		Value: "./config/genesis.json",
+	}
+	// nodesFile defines a flag for the path of the initial nodes file.
+	nodesFile = cli.StringFlag{
+		Name:  "nodesSetup-file",
+		Usage: "The node will extract initial nodes info from the nodesSetup.json",
+		Value: "./config/nodesSetup.json",
+	}
+	// txSignSk defines a flag for the path of the single sign private key used when starting the node
+	txSignSk = cli.StringFlag{
+		Name:  "tx-sign-sk",
+		Usage: "Private key that the node will load on startup and will sign transactions - temporary until we have a wallet that can do that",
+		Value: "",
+	}
+	// sk defines a flag for the path of the multi sign private key used when starting the node
+	sk = cli.StringFlag{
+		Name:  "sk",
+		Usage: "Private key that the node will load on startup and will sign blocks",
+		Value: "",
+	}
+	// configurationFile defines a flag for the path to the main toml configuration file
+	configurationFile = cli.StringFlag{
+		Name:  "config",
+		Usage: "The main configuration file to load",
+		Value: "./config/config.toml",
+	}
+	// configurationEconomicsFile defines a flag for the path to the economics toml configuration file
+	configurationEconomicsFile = cli.StringFlag{
+		Name:  "configEconomics",
+		Usage: "The economics configuration file to load",
+		Value: "./config/economics.toml",
+	}
+	// configurationPreferencesFile defines a flag for the path to the preferences toml configuration file
+	configurationPreferencesFile = cli.StringFlag{
+		Name:  "configPreferences",
+		Usage: "The preferences configuration file to load",
+		Value: "./config/prefs.toml",
+	}
+	// p2pConfigurationFile defines a flag for the path to the toml file containing P2P configuration
+	p2pConfigurationFile = cli.StringFlag{
+		Name:  "p2pconfig",
+		Usage: "The configuration file for P2P",
+		Value: "./config/p2p.toml",
+	}
+	// p2pConfigurationFile defines a flag for the path to the toml file containing P2P configuration
+	serversConfigurationFile = cli.StringFlag{
+		Name:  "serversconfig",
+		Usage: "The configuration file for servers confidential data",
+		Value: "./config/server.toml",
+	}
+	// port defines a flag for setting the port on which the node will listen for connections
+	port = cli.IntFlag{
+		Name:  "port",
+		Usage: "Port number on which the application will start",
+		Value: 0,
+	}
+	// profileMode defines a flag for profiling the binary
+	// If enabled, it will open the pprof routes over the default gin rest webserver.
+	// There are several routes that will be available for profiling (profiling can be analyzed with: go tool pprof):
+	//  /debug/pprof/ (can be accessed in the browser, will list the available options)
+	//  /debug/pprof/goroutine
+	//  /debug/pprof/heap
+	//  /debug/pprof/threadcreate
+	//  /debug/pprof/block
+	//  /debug/pprof/mutex
+	//  /debug/pprof/profile (CPU profile)
+	//  /debug/pprof/trace?seconds=5 (CPU trace) -> being a trace, can be analyzed with: go tool trace
+	// Usage: go tool pprof http(s)://ip.of.the.server/debug/pprof/xxxxx
+	profileMode = cli.BoolFlag{
+		Name:  "profile-mode",
+		Usage: "Boolean profiling mode option. If set to true, the /debug/pprof routes will be available on the node for profiling the application.",
+	}
+	// txSignSkIndex defines a flag that specifies the 0-th based index of the private key to be used from initialBalancesSk.pem file
+	txSignSkIndex = cli.IntFlag{
+		Name:  "tx-sign-sk-index",
+		Usage: "Single sign private key index specifies the 0-th based index of the private key to be used from initialBalancesSk.pem file.",
+		Value: 0,
+	}
+	// skIndex defines a flag that specifies the 0-th based index of the private key to be used from initialNodesSk.pem file
+	skIndex = cli.IntFlag{
+		Name:  "sk-index",
+		Usage: "Private key index specifies the 0-th based index of the private key to be used from initialNodesSk.pem file.",
+		Value: 0,
+	}
+	// gopsEn used to enable diagnosis of running go processes
+	gopsEn = cli.BoolFlag{
+		Name:  "gops-enable",
+		Usage: "Enables gops over the process. Stack can be viewed by calling 'gops stack <pid>'",
+	}
+	// numOfNodes defines a flag that specifies the maximum number of nodes which will be used from the initialNodes
+	numOfNodes = cli.Uint64Flag{
+		Name:  "num-of-nodes",
+		Usage: "Number of nodes specifies the maximum number of nodes which will be used from initialNodes list exposed in nodesSetup.json file",
+		Value: math.MaxUint64,
+	}
+	// storageCleanup defines a flag for choosing the option of starting the node from scratch. If it is not set (false)
+	// it starts from the last state stored on disk
+	storageCleanup = cli.BoolFlag{
+		Name:  "storage-cleanup",
+		Usage: "If set the node will start from scratch, otherwise it starts from the last state stored on disk",
+	}
+
+	// restApiInterface defines a flag for the interface on which the rest API will try to bind with
+	restApiInterface = cli.StringFlag{
+		Name: "rest-api-interface",
+		Usage: "The interface address and port to which the REST API will attempt to bind. " +
+			"To bind to all available interfaces, set this flag to :8080",
+		Value: "localhost:8080",
+	}
+
+	// restApiDebug defines a flag for starting the rest API engine in debug mode
+	restApiDebug = cli.BoolFlag{
+		Name:  "rest-api-debug",
+		Usage: "Start the rest API engine in debug mode",
+	}
+
+	// networkID defines the version of the network. If set, will override the same parameter from config.toml
+	networkID = cli.StringFlag{
+		Name:  "network-id",
+		Usage: "The network version, overriding the one from config.toml",
+		Value: "",
+	}
+
+	// nodeDisplayName defines the friendly name used by a node in the public monitoring tools. If set, will override
+	// the NodeDisplayName from config.toml
+	nodeDisplayName = cli.StringFlag{
+		Name:  "display-name",
+		Usage: "This will represent the friendly name in the public monitoring tools. Will override the config.toml one",
+		Value: "",
+	}
+
+	// usePrometheus joins the node for prometheus monitoring if set
+	usePrometheus = cli.BoolFlag{
+		Name:  "use-prometheus",
+		Usage: "Will make the node available for prometheus and grafana monitoring",
+	}
+
+	//useLogView is used when termui interface is not needed.
+	useLogView = cli.BoolFlag{
+		Name:  "use-log-view",
+		Usage: "will not enable the user-friendly terminal view of the node",
+	}
+
+	// initialBalancesSkPemFile defines a flag for the path to the ...
+	initialBalancesSkPemFile = cli.StringFlag{
+		Name:  "initialBalancesSkPemFile",
+		Usage: "The file containing the secret keys which ...",
+		Value: "./config/initialBalancesSk.pem",
+	}
+
+	// initialNodesSkPemFile defines a flag for the path to the ...
+	initialNodesSkPemFile = cli.StringFlag{
+		Name:  "initialNodesSkPemFile",
+		Usage: "The file containing the secret keys which ...",
+		Value: "./config/initialNodesSk.pem",
+	}
+	// logLevel defines the logger level
+	logLevel = cli.StringFlag{
+		Name:  "logLevel",
+		Usage: "This flag specifies the logger level",
+		Value: "*:INFO",
+	}
+	// disableAnsiColor defines if the logger subsystem should prevent displaying ANSI colors
+	disableAnsiColor = cli.BoolFlag{
+		Name:  "disable-ansi-color",
+		Usage: "This flag specifies that the log output should not use ANSI colors",
+	}
+	// bootstrapRoundIndex defines a flag that specifies the round index from which node should bootstrap from storage
+	bootstrapRoundIndex = cli.Uint64Flag{
+		Name:  "bootstrap-round-index",
+		Usage: "Bootstrap round index specifies the round index from which node should bootstrap from storage",
+		Value: math.MaxUint64,
+	}
+	// enableTxIndexing enables transaction indexing. There can be cases when it's too expensive to index all transactions
+	//  so we provide the command line option to disable this behaviour
+	enableTxIndexing = cli.BoolTFlag{
+		Name:  "tx-indexing",
+		Usage: "Enables transaction indexing. There can be cases when it's too expensive to index all transactions so we provide the command line option to disable this behaviour",
+	}
+
+	// workingDirectory defines a flag for the path for the working directory.
+	workingDirectory = cli.StringFlag{
+		Name:  "working-directory",
+		Usage: "The node will store here DB, Logs and Stats",
+		Value: "",
+	}
+
+	// destinationShardAsObserver defines a flag for the prefered shard to be assigned to as an observer.
+	destinationShardAsObserver = cli.StringFlag{
+		Name:  "destination-shard-as-observer",
+		Usage: "The preferred shard as an observer",
+		Value: "",
+	}
+)
+
+// appVersion should be populated at build time using ldflags
+// Usage examples:
+// linux/mac:
+//            go build -i -v -ldflags="-X main.appVersion=$(git describe --tags --long --dirty)"
+// windows:
+//            for /f %i in ('git describe --tags --long --dirty') do set VERS=%i
+//            go build -i -v -ldflags="-X main.appVersion=%VERS%"
+var appVersion = "undefined"
+
+func main() {
+	app := cli.NewApp()
+	cli.AppHelpTemplate = nodeHelpTemplate
+	app.Name = "Elrond Node CLI App"
+	app.Version = fmt.Sprintf("%s/%s/%s-%s", appVersion, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+	app.Flags = []cli.Flag{
+		genesisFile,
+		nodesFile,
+		port,
+		configurationFile,
+		configurationEconomicsFile,
+		configurationPreferencesFile,
+		p2pConfigurationFile,
+		txSignSk,
+		sk,
+		profileMode,
+		txSignSkIndex,
+		skIndex,
+		numOfNodes,
+		storageCleanup,
+		initialBalancesSkPemFile,
+		initialNodesSkPemFile,
+		gopsEn,
+		serversConfigurationFile,
+		networkID,
+		nodeDisplayName,
+		restApiInterface,
+		restApiDebug,
+		disableAnsiColor,
+		logLevel,
+		usePrometheus,
+		useLogView,
+		bootstrapRoundIndex,
+		enableTxIndexing,
+		workingDirectory,
+		destinationShardAsObserver,
+	}
+	app.Authors = []cli.Author{
+		{
+			Name:  "The Elrond Team",
+			Email: "contact@elrond.com",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		return startNode(c, app.Version)
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Printf("error: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+func startNode(_ *cli.Context, version string) error {
+	fmt.Printf("application is now running, version %s\n", version)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+	fmt.Println("terminating at user's signal...")
+
+	return nil
+}
